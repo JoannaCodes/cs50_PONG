7a5ec3285f0ecc7b8196858954bded9b0460bd1b
JoannaCodes <joannalara21@gmail.com>
2020-12-10 17:05:27 +0800

    Modified PONG

---
+++ cs50_PONG/Ball.lua
@@ -0,0 +1,50 @@
+Ball = Class{}
+
+--Initialization
+function Ball:init(x, y, radius, segments)
+    self.x = x
+    self.y = y
+    self.radius = radius
+    self.segments = segments
+
+    --Ball's velocity
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(100,150)
+end
+
+--Reset
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 -2
+
+    --Ball's velocity
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
+end
+
+--Movement
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+--Ball Collision(--->>>New)
+function Ball:collision(paddle)
+    --No collision
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.radius then
+        return false
+    end
+
+    --No collision
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.radius then
+        return false
+    end
+
+    --There is collision
+    return true
+end
+
+--Output
+function Ball:render()
+    love.graphics.circle('fill', self.x, self.y, self.radius, self.segments)
+end
